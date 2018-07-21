a9bc87af3a7df8403ba22daa15ca6fe08f39633b
mhaut <juanmariohaut@unex.es>
2018-07-22 00:12:31 +0200

    upload files

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 
 su - hadoop
-cat hadoop_bashrc >>  ~/.bashrc
-cp core-site.xml /usr/local/hadoop/etc/hadoop/core-site.xml
-cp hdfs-site.xml /usr/local/hadoop/etc/hadoop/hdfs-site.xml
-cp mapred-site.xml /usr/local/hadoop/etc/hadoop/mapred-site.xml
-cp yarn-site.xml /usr/local/hadoop/etc/hadoop/yarn-site.xml
-cp slaves /usr/local/hadoop/etc/hadoop/slaves
+cat /home/user/software/igarss_tutorial/hadoop_bashrc >>  /home/hadoop/.bashrc
+cp /home/user/software/igarss_tutorial/core-site.xml /usr/local/hadoop/etc/hadoop/core-site.xml
+cp /home/user/software/igarss_tutorial/hdfs-site.xml /usr/local/hadoop/etc/hadoop/hdfs-site.xml
+cp /home/user/software/igarss_tutorial/mapred-site.xml /usr/local/hadoop/etc/hadoop/mapred-site.xml
+cp /home/user/software/igarss_tutorial/yarn-site.xml /usr/local/hadoop/etc/hadoop/yarn-site.xml
+cp /home/user/software/igarss_tutorial/slaves /usr/local/hadoop/etc/hadoop/slaves
 
 
 
