44449fa1ed889393f3c0fd93c1ccb3b0ff285a99
a <a>
2018-07-22 12:58:56 +0200

    changes

--- hadoop/script.sh
+++ hadoop/script.sh
@@ -1,10 +1,9 @@
 
-su - hadoop
-cat /home/hadoop/igarss_tutorial/hadoop_bashrc >>  /home/hadoop/.bashrc
-cp /home/hadoop/igarss_tutorial/core-site.xml /usr/local/hadoop/etc/hadoop/core-site.xml
-cp /home/hadoop/igarss_tutorial/hdfs-site.xml /usr/local/hadoop/etc/hadoop/hdfs-site.xml
-cp /home/hadoop/igarss_tutorial/mapred-site.xml /usr/local/hadoop/etc/hadoop/mapred-site.xml
-cp /home/hadoop/igarss_tutorial/yarn-site.xml /usr/local/hadoop/etc/hadoop/yarn-site.xml
-cp /home/hadoop/igarss_tutorial/slaves /usr/local/hadoop/etc/hadoop/slaves
-mkdir -p /usr/local/hadoop/hadoop_data/hdfs/namenode 
+cat /home/hadoop/igarss_tutorial/hadoop/hadoop_bashrc >>  /home/hadoop/.bashrc
+cp /home/hadoop/igarss_tutorial/hadoop/core-site.xml /usr/local/hadoop/etc/hadoop/core-site.xml
+cp /home/hadoop/igarss_tutorial/hadoop/hdfs-site.xml /usr/local/hadoop/etc/hadoop/hdfs-site.xml
+cp /home/hadoop/igarss_tutorial/hadoop/mapred-site.xml /usr/local/hadoop/etc/hadoop/mapred-site.xml
+cp /home/hadoop/igarss_tutorial/hadoop/yarn-site.xml /usr/local/hadoop/etc/hadoop/yarn-site.xml
+cp /home/hadoop/igarss_tutorial/hadoop/slaves /usr/local/hadoop/etc/hadoop/slaves
+mkdir -p /usr/local/hadoop/hadoop_data/hdfs/namenode
 mkdir -p /usr/local/hadoop/hadoop_data/hdfs/datanode
--- spark/script.sh
+++ spark/script.sh
@@ -1,4 +1,1 @@
-cat /home/user/igarss_tutorial/spark_bashrc >>  /home/user/.bashrc
-
-cp slaves /home/user/spark/conf/slaves
-cp log4j.properties /home/user/spark/conf/log4j.properties
+cat /home/hadoop/igarss_tutorial/hadoop_bashrc >>  /home/hadoop/.bashrc
